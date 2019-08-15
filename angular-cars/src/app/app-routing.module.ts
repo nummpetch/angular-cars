d5f64093c6d7aa8e34b3caa65c2c72221fd57ac8
Numpetch <petch.skuberg@gmail.com>
2019-08-15 16:13:06 +0700

    route

--- angular-cars/src/app/app-routing.module.ts
+++ angular-cars/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'/', component:HomeComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+
+  imports: [RouterModule.forRoot(routes),
+  BrowserModule,
+  CommonModule
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- angular-cars/src/app/app.module.ts
+++ angular-cars/src/app/app.module.ts
@@ -1,20 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes }from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
+
+
+const appRoutes:Routes=[ 
+  // {path:'', component:AppComponent},
+  {path:'login', component:LoginComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    LoginComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
+
 })
+
 export class AppModule { }
